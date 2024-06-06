083612430ede0bd4978e8669121ab0c58a7ed56b
verahk <verahk@uio.no>
2024-06-06 17:35:30 +0200

    add option for forcing partition to be regular

--- R/score_from_lookup.R
+++ R/score_from_lookup.R
@@ -11,7 +11,7 @@
 #' @param j node to be scored
 #'
 #' @return score of node `j` given `parentnodes` and optimal CSI-structure 
-score_from_lookup <- function(data, levels, nlev = lengths(levels), j, parentnodes, ess, method, lookup) {
+score_from_lookup <- function(data, levels, nlev = lengths(levels), j, parentnodes, ess, method, regular, lookup) {
   
   if (length(parentnodes) > 1) {
     parentnodes <- sort(parentnodes)
@@ -19,7 +19,7 @@ score_from_lookup <- function(data, levels, nlev = lengths(levels), j, parentnod
     score <- lookup[[method]][[id]]
     # compute score and store in lookup-table
     if (is.null(score)) {
-      score <- compute_local_bdeu_score_from_data(data, levels, nlev, j, parentnodes, ess,  method, lookup)
+      score <- compute_local_bdeu_score_from_data(data, levels, nlev, j, parentnodes, ess,  method, regular, lookup)
       lookup[[method]][[id]] <- score
     }
     return(score)
