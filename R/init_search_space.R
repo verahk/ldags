140eccf260bad9aa92a6fab58d1f44a4528261bd
verahk <verahk@uio.no>
2024-06-10 20:47:35 +0200

    add functions for sampling DAGs with local structure

---
+++ R/init_search_space.R
@@ -0,0 +1,60 @@
+
+
+#' Initialize search space
+#' 
+#' Wrapper around structural learning routines ([bnlearn::hc] and [pcalg::pc]),
+#' intended to learn a start-space for the [Bidag::iterativeMCMC] procedure. 
+#' Forces the maximal number of parents (`hardlimit`) to be respected, 
+#' by re-running the structure learning procedure with stricter add-edge-policies
+#' (`maxp` and `alpha`, respectively) until a structure that satifies the `hardlimit`
+#' criteria is inferred.
+#' 
+#' @inheritParams sample_dags
+#' @param maxp (integer)
+#'  Maximal number of parents in CPDAG learned by [bnlearn::hc]. 
+#'  Ignored if not `algo_int=="hc"` or `algo_init == "hcskel"`.
+#' @param alpha (numeric)
+#'  Significance level for the conditional independencies test in [pcalg::pc].
+#'  Ignored if not `algo_int=="pc"` or `algo_init == "pcskel"`. 
+#' 
+#' @return a `n-by-n` adjacency matrix
+#' @export
+#'
+#' @examples
+#' @seealso \link{sample_dags}
+init_search_space <- function(scorepar, algo_init, hardlimit, maxp = hardlimit, alpha = .05, verbose) {
+
+  if (algo_init == "hc") {
+    df <- data.frame(apply(scorepar$data, 2, factor, exclude = NULL, simplify = FALSE))
+    fit  <- bnlearn:::hc(df, maxp = maxp)
+    startspace <- bnlearn::amat(fit)
+  } else if (algo_init == "hcskel") {
+    df <- data.frame(apply(scorepar$data, 2, factor, exclude = NULL, simplify = FALSE))
+    fit  <- bnlearn:::hc(df, maxp = maxp)
+    startspace <- bnlearn::amat(fit)
+    startspace <- startspace + t(startspace)
+  } else if (algo_init == "pc") {
+    suffStat <- list(dm = scorepar$data, nlev = scorepar$Cvec, adaptDF = FALSE)
+    fit <- pcalg::pc(suffStat, pcalg::disCItest, alpha = alpha, 
+                     labels = colnames(scorepar$data), verbose = verbose)
+    startspace <- as(fit@graph, "matrix")
+  } else if (algo_init == "pcskel") {
+    suffStat <- list(dm = scorepar$data, nlev = scorepar$Cvec, adaptDF = FALSE)
+    fit <- pcalg::skeleton(suffStat, pcalg::disCItest, alpha = alpha, 
+                           labels = colnames(scorepar$data), verbose = verbose)
+    startspace <- as(fit@graph, "matrix")
+  } else if (algo_init == "BiDAG") {
+    fit <- try(iterativeMCMC(scorepar, hardlimit = hardlimit, alpha = alpha))
+  }
+
+  if (! any(colSums(startspace) > hardlimit)) {
+    return(startspace)
+  } else if (maxp > 1 && alpha > .001) {
+    init_search_space(scorepar, algo_init, hardlimit, maxp = maxp-1, alpha = alpha/2, verbose)
+  } else {
+    cat("\nCould not find startspace satisfying `hardlimit`\n")
+    cat(sprintf("algo_init: %s hardlimit: %s maxp: %s alpha: %s", algo_init, hardlimit, maxp, alpha))
+    stop()
+  }
+}
+
--- R/sample_dags.R
+++ R/sample_dags.R
@@ -1,138 +1,45 @@
 
 
-#' Sample DAGs with local structure
-#' 
-#' Apply hybrid sampling scheme provided by the `BiDAG`-package with user-defined
-#' scoring functions to sample DAGs with optimized local structure.
-#' Use `bnlearn::hc` to definine a CPDAG for the initial search space, limiting 
-#' the maximum number of parents.
+#' Title
 #'
-#' @param data (integer matrix)
-#' @param nlev (integer vector)
-#' @param algo (character)
-#'  which MCMC-scheme to employ for sampling DAGs given a search space. Either
-#'  `"partition"` or `"order"`. See [BiDAG::sampleBN].
-#' @param ess (numeric)
-#' @param edgepf (numeric)
-#'  edge penalty
+#' @param scorepar (object of class [BiDAG::scoreparameters])
+#'  See \link{define_scorepar}.
+#' @param algo_init (character)
+#'  Name of optimization routine for local structure. 
+#'  See \link{optimize_partition}.
+#' @param algo_sample (character)
+#'  Name of MCMC implementation. Either `partition` or `order`. 
+#'  See [BiDAG::sampleBN].
 #' @param hardlimit (integer)
-#'  maximum number of parents.
-#' @param local_struct (character)
-#'  greedy optimization routine to optimize the local structure. Either `tree` or 
-#'  `ldag`. If `NULL` (default), no local structure is optimized.
+#'  Maximal number of parents allowed. See [BiDAG::learnBN].
 #' @param verbose (logical)
-#' @param lookup (environment)
-#'  a environment in which the optimized local structures is stored. 
-#'  Generated e.g. with [rlang::new_environment]. See [score_from_lookup].
-#' 
-#' @return 
+#' @return
 #' @export
 #'
 #' @examples
-#' bn <- example_bn("LDAG10")
-#' nlev <- vapply(bn, function(x) dim(x$prob)[1], integer(1))
-#' data <- bida:::sample_data_from_bn(bn, 1000)
-#' lookup <- rlang::new_environment() 
-#' 
-#' smpl_tree <- sample_dags(data, nlev, algo = "order", local_struct = "tree", verbose = T, lookup = lookup)
-#' smpl_ldag <- sample_dags(data, nlev, algo = "order", local_struct = "ldag", verbose = T, lookup = lookup)
-#' smpl_dag  <- sample_dags(data, nlev, algo = "order", local_struct = NULL, verbose = T, lookup = lookup)    
-#' 
-#' edgep <- cbind(tree = c(BiDAG::edgep(smpl_tree)), 
-#'                ldag = c(BiDAG::edgep(smpl_ldag)),
-#'                dag  = c(BiDAG::edgep(smpl_dag)))
-#'                
-#' pairs(edgep)
-#' 
-#' profvis::profvis(sample_dags(data, nlev, algo = "order", local_struct = "ldag", verbose = T, lookup = lookup))
-
-
-sample_dags <- function(data, nlev, algo = "order", ess = 1, edgepf = 2, hardlimit = 5, local_struct = NULL, verbose = FALSE, lookup = NULL) {
-  
-  hardlimit <- min(ncol(data)-1, hardlimit)
-  algo <- match.arg(algo, c("order", "partition"))
-  
-  # genereate data.frame, removing unobserved levels in data, as required by BiDAG
-  df <- data.frame(apply(data, 2, factor, exclude = NULL, simplify = FALSE))
-  
-  # define scoreparameters 
-  if (! is.null(local_struct) && local_struct %in% c("tree", "ldag")) {
-
-    scorepar <- BiDAG::scoreparameters("usr", 
-                                       data = data, 
-                                       usrpar = list(pctesttype = "bdecat"))
-    
-    # additional parameters for user-specified score params 
-    scorepar$ess  <- ess 
-    scorepar$edgepf <- edgepf
-    scorepar$Cvec <- nlev
-    scorepar$levels <- lapply(nlev-1, seq.int, from = 0)
-    scorepar$local_struct <- local_struct
-    
-    # define environment in which to store optimized local structures
-    if (!is.null(lookup)) {
-      tmp <- list()
-      assign(local_struct, tmp, lookup)
-      scorepar$lookup <- lookup
-    }
-    
-    
-    # define scoring function
-    usrDAGcorescore <- function(j, parentnodes, n, scorepar) {
-      score_from_lookup(scorepar$data, 
-                        levels = scorepar$levels, 
-                        nlev = scorepar$Cvec, 
-                        j, 
-                        parentnodes, 
-                        ess = scorepar$ess,
-                        method = scorepar$local_struct, 
-                        lookup = scorepar$lookup) - length(parentnodes)*log(scorepar$edgepf)
-    }
-    assignInNamespace("usrDAGcorescore", usrDAGcorescore, ns = "BiDAG")
-  } else {
-    scorepar <- BiDAG::scoreparameters("bdecat", 
-                                       data = df, 
-                                       bdecatpar = list(chi = ess, 
-                                                        edgepf = edgepf))
-  }
+sample_dags <- function(scorepar, algo_init = "pcskel", algo_sample = "order", hardlimit = 5, verbose = F) {
   
   tic <- Sys.time()
-  # learn skeleton
-  # if (verbose) cat("\nLearn skeleton using pcalg::skeleton")
-  # skel <- pcalg::skeleton(suffStat = list(dm = data, adaptDF = FALSE),
-  #                         indepTest = pcalg::disCItest, alpha = 0.05,
-  #                         labels = colnames(data), verbose = verbose, m.max = hardlimit) 
-  # tic <- c(tic, skel = Sys.time())
-  if (verbose) cat("\nLearn initial CPDAG using bnlearn::hc\n")
-  cpdag <- bnlearn:::hc(df, maxp = hardlimit)
-  skel <- bnlearn::amat(cpdag)
-  tic <- c(tic, hc = Sys.time())
   
-  # define search space 
-  if (verbose) cat("\nDefine search space using BiDAG::learnBN\n")
-  iterfit <- BiDAG::learnBN(scorepar, "orderIter", hardlimit = hardlimit, scoreout = T, verbose = verbose, startspace = skel)
-  tic <- c(tic, learnBN = Sys.time())
-
-  # sample DAGs 
-  if (verbose) cat("\nSample DAGs using BiDAG::sampleBN\n")
-  scoretab <- BiDAG::getSpace(iterfit)
-  stopifnot(!is.null(scoretab$adjacency))
-  startskeleton <- scoretab$adjacency
-  smpl     <- BiDAG::sampleBN(scorepar, algo, scoretable = scoretab, verbose = verbose)
-                              #plus1 = FALSE,                   #
-                              #startspace = scoretab$adjacency) # should be ignored when scoretable is given
-
-   # if (algo == "order") {
-   #   smpl <- BiDAG:::orderMCMC(scorepar, scoretable = scoretab, MAP = FALSE, plus1 = F, verbose = verbose)
-   # } else if (algo == "partition") {
-   #   smpl <- BiDAG:::partitionMCMC(scorepar, scoretable = scoretab, verbose = T)
-   # }
-   # 
-  tic <- c(tic, sampleBN = Sys.time())
+  # init search space 
+  if (verbose) cat("\nInit search space:")
+  startspace <- init_search_space(scorepar, algo_init, hardlimit, verbose = verbose)
+  tic <- c(tic, init = Sys.time())
+  
+  # expand search space
+  if (verbose) cat("\nExpand search space using BiDAG::iterativeMCMC:")
+  #iterfit <- BiDAG::iterativeMCMC(scorepar, hardlimit = hardlimit, startspace = startspace, scoreout = T, verbose = verbose)
+  iterfit <- BiDAG::learnBN(scorepar, "orderIter", hardlimit = 5, startspace = startspace, scoreout = T, verbose = verbose)
+  tic <- c(tic, expand = Sys.time())
+  
+  # run MCMC 
+  if (verbose) cat("\nSample DAGs using BiDAG::sampleBN:\n")
+  smpl <- BiDAG::sampleBN(scorepar, algo = algo_sample, scoretable = BiDAG::getSpace(iterfit), verbose = verbose)
+  tic  <- c(tic, sample = Sys.time())
   
   # add time-tracking as an attribute
   attr(smpl, "toc") <- diff(tic)
-  attr(smpl, "params") <- list(algo = algo, ess = ess, edgepf = edgepf, hardlimit = hardlimit, local_struct = m)
   
   return(smpl)
+  
 }
